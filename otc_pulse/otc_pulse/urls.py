100eb2dffdba086337fd539d283601640bb043e3
Shanna <sg0989463@otc.edu>
2026-05-10 18:43:59 -0500

    Fix: Add missing migration files and update URLs and configuration

--- otc_pulse/otc_pulse/urls.py
+++ otc_pulse/otc_pulse/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 urlpatterns = [
+    path('health/', lambda request: HttpResponse('OK'), name='health'),
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('clubs/', include('clubhouse.urls')),
